99b1373e6131cd1a0fff873007b3f15f3e714cbf
Abishalini Sivaraman <abi.gpuram@gmail.com>
2020-04-20 13:37:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='packaging_test',
       version='0.1',
       description='The funniest joke in the world',
-      url='https://github.com/ssvillareal/local_siml.git',
+      url='https://github.com/Abishalini/local_siml.git',
       #author='Flying Circus',
       #author_email='flyingcircus@example.com',
       author='SSV',
